c97d90ff2170d6fab11841268ef5d9d866ff6521
Priyanshu Kumar Singh <pks9918705@gmail.com>
2023-04-28 00:22:41 +0530

    contact list is created till Middleware lecture

--- index.js
+++ index.js
@@ -2,22 +2,17 @@ const express = require('express')
 const app = express()
 const port = 8000
 const path = require('path')
+//parser
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: false}));
 
 
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
-
+// sending data from EJS to the server
 //! a variable - available globally
 var contactList = [
-    {
-        name: 'John', email: 'kenaa@example.com', phone: '1234567890'
-    },
-    {
-        name: 'Jane', email: 'kenaa@example.com', phone: '1234567890'
-    },
-    {
-        name: 'Jim', email: 'kenaa@example.com', phone: '1234567890'
-    }
+     
 ]
 
 app.get('/', (req, res) => {
@@ -29,6 +24,32 @@ app.get('/', (req, res) => {
         // 'contact_List' is array of contact in js file while 'contactList' is array of contact that are available to ejs file
     })
 });
+app.get('/practice', (req, res) => {
+    return res.render('practice',{
+        title: 'Practice'
+    })
+});
+
+
+app.post('/create-contact', (req, res) => {
+
+    // return res.redirect('/practice');
+    // console.log(req.body)
+    // req k pass ek body naame ka object have jiska property hai 'name','email','phone'(key hain saray)
+
+    // method 1 
+    // const newContact = {
+    //     name: req.body.name,
+    //     email: req.body.email,
+    //     phone: req.body.phone
+    // }
+    // contactList.push(newContact);
+
+    // method 2
+    contactList.push(req.body)
+
+    return res.redirect('/');
+})
 
 
 
